4b0e57bc9b6af0ffe4ac5a34117d815398c6ce87
Angel Tapia <angeltapia1805@gmail.com>
2024-09-19 13:39:07 -0600

    Refactor routes.py to add new endpoints for retrieving student and subject information

--- api/routes.py
+++ api/routes.py
@@ -11,3 +11,41 @@ router = APIRouter()
 @router.get("/")
 async def health_check():
     return "Everything is fine!"
+
+@router.get("/alumnos/{plan}/{subjectId}/{group}", response_model=MateriaAlumnos)
+async def getAlumnos(plan: str, subjectId: str, group: str):
+    response = None
+    if plan == "420":
+        response = getListaAlumnos420(subjectId, group)
+    elif plan == "430":
+        response = getListaAlumnos430(subjectId, group)
+    elif plan == "440":
+        response = getListaAlumnos440(subjectId, group)
+    else:
+        raise HTTPException(status_code=404, detail="Plan not found")
+    if response is None:
+        raise HTTPException(status_code=404, detail="Materia not found")
+    return response
+
+@router.get("/materias/{employee_id}", response_model=Profesor)
+async def getMaterias(employee_id: str):
+    response = getListaMaterias(employee_id)
+    if response is None:
+        raise HTTPException(status_code=404, detail="Employee not found")
+    return response
+
+@router.post("/pdf", response_class=FileResponse)
+async def create_pdf(request: PdfRequest):
+    pdf_file_path = generate_pdf(
+        request.alumno,
+        request.materiaAlumno,
+        request.plan,
+        request.profesor,
+        request.calificacionIncorrecta,
+        request.calificacionCorrecta,
+        request.motivo,
+        request.academia,
+        request.nombreCoordinador,
+    )
+    return FileResponse(pdf_file_path, media_type='application/pdf', filename="output.pdf")
+
